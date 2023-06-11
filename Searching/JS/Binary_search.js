698998a2d6ca750c7546732b06409bfd2ff1fe6b
ZadeAbhishek <zabhidoc@gmail.com>
2023-06-11 10:09:04 +0000

    Add new Ignore

--- Searching/JS/Binary_search.js
+++ Searching/JS/Binary_search.js
@@ -4,9 +4,9 @@ function BinarySearch(arr, target, high, low) {
     let result;
     if (high == low && arr[low] == target) return low;
     if (low > high) return -1;
-    let mid = (high + low) / 2;
+    let mid = Math.round((high + low) / 2);
     if (arr[mid] == target) return mid;
-    if (arr[mid] > target) result = BinarySearch(arr, target, mid, low);
+    if (arr[mid] > target) result = BinarySearch(arr, target, mid-1, low);
     else result = BinarySearch(arr, target, high, mid + 1);
     return result;
 
@@ -15,7 +15,7 @@ function BinarySearch(arr, target, high, low) {
 
 function init() {
     let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-    let result = BinarySearch(arr, 13, arr.length - 1, 0); //(array,target,high,low)
+    let result = BinarySearch(arr, 1, arr.length - 1, 0); //(array,target,high,low)
     if (result >= 0) console.log(`Element Found at ${result}`);
     else console.log("Element not found");
 }

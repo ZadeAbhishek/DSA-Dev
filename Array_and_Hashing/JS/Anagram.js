e4356ddd93570a3ad4f2a7e92bc66b440ecaa37e
ZadeAbhishek <zabhidoc@gmail.com>
2023-06-27 16:38:04 +0000

    Update on Tue Jun 27 16:38:04 UTC 2023

--- Array_and_Hashing/JS/Anagram.js
+++ Array_and_Hashing/JS/Anagram.js
@@ -10,15 +10,26 @@ function Anagram(s, r) {
     for (let c of r) Buffer[c.charCodeAt(c)]--; //O(n)
 
     for (let c of Buffer) //O(1)
-        if (c > 0) return false;
+        if (c != 0) return false;
 
     return true;
 }
 
+// Not Working
+// function Anagram_2(s,r){
+//      if(s.length !== r.length) return false;
+//      let t = 0 , y = 0 , m = 0 , n = 0 , z = 0;
+//      for(let c of s) t += c.charCodeAt(c), m = m ^ c.charCodeAt(c);
+//      for(let c of r) y += c.charCodeAt(c), n = n ^ c.charCodeAt(c);
+//      for(let i = 0 ; i < s.length ; i++) z += s.charCodeAt(i) - r.charCodeAt(i);
+//      console.log(t,y,m,n,z); 
+//      let res = t ^ y;
+//      return (res===0?true:false)
+// }
 
 function init() {
-    let s = "zipri";
-    let r = "prizi";
+    let s = "af";
+    let r = "be";
     let result = Anagram(s, r);
     console.log(result);
 }

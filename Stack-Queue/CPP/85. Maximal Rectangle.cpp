f32229664db61032f34d6b2590ac86f63c2ff6f1
ZadeAbhishek <zabhidoc@gmail.com>
2023-08-15 10:57:06 +0000

    Partition DP Update on Tue Aug 15 10:57:06 UTC 2023

--- Stack-Queue/CPP/85. Maximal Rectangle.cpp
+++ Stack-Queue/CPP/85. Maximal Rectangle.cpp
@@ -1,46 +1,62 @@
-/**
- * Given a rows x cols binary matrix filled with 0's and 1's, 
- * find the largest rectangle containing only 1's and return its area.
- * Input: matrix = [["1","0","1","0","0"],
- *                  ["1","0","1","1","1"],
- *                  ["1","1","1","1","1"],
- *                  ["1","0","0","1","0"]]
-Output: 6
-Explanation: The maximal rectangle is shown in the above picture.
- * 
-*/
+#include <iostream>
+#include <vector>
+#include <stack>
+#include<climits>
+using namespace std;
 
 class Solution {
 public:
-int largestRectangleAreaOptimal1Pass(vector<int>& heights){
-    stack<int> s;
-    int uB = heights.size(); // upper Bound
-    int max_Area = INT_MIN;
-    for(int i = 0 ; i <= uB ; i++){
-        while(!s.empty() && (i == uB || heights[s.top()] > heights[i])){
-             int h = heights[s.top()]; // height
-             s.pop();
-             int w; // width
-             if(s.empty()) w = i;
-             else w = i - s.top() - 1;
-             int area = h * w;
-             max_Area = max(max_Area,area);
-        }
-        s.push(i);
-    }
-    return max_Area;
-}
-    int maximalRectangle(vector<vector<char>>& matrix) {
-        vector<int> currHeights(matrix[0].size(),0);
+    // Function to find the largest rectangle area in a histogram using an optimal one-pass approach.
+    int largestRectangleAreaOptimal1Pass(vector<int>& heights) {
+        stack<int> s;
+        int uB = heights.size(); // upper Bound
         int max_Area = INT_MIN;
-        for(int i = 0 ; i < matrix.size() ; i++){
-            for(int j = 0 ; j < matrix[i].size(); j++){
-                if(matrix[i][j] == '1') currHeights[j] += 1;
-                else currHeights[j] = 0; 
+        for (int i = 0; i <= uB; i++) {
+            while (!s.empty() && (i == uB || heights[s.top()] > heights[i])) {
+                int h = heights[s.top()]; // height
+                s.pop();
+                int w; // width
+                if (s.empty()) w = i;
+                else w = i - s.top() - 1;
+                int area = h * w;
+                max_Area = max(max_Area, area);
+            }
+            s.push(i);
+        }
+        return max_Area;
+    }
+
+    // Function to find the maximal rectangle area containing only 1's in a binary matrix.
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        vector<int> currHeights(matrix[0].size(), 0);
+        int max_Area = INT_MIN;
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[i].size(); j++) {
+                if (matrix[i][j] == '1')
+                    currHeights[j] += 1;
+                else
+                    currHeights[j] = 0;
             }
             int curr_max_Area = largestRectangleAreaOptimal1Pass(currHeights);
-            max_Area = max(max_Area,curr_max_Area);
+            max_Area = max(max_Area, curr_max_Area);
         }
         return max_Area;
     }
 };
+
+int main() {
+    Solution solution;
+
+    // Example input: a binary matrix representing the land with 1's and 0's
+    vector<vector<char>> matrix = {
+        {'1', '0', '1', '0', '0'},
+        {'1', '0', '1', '1', '1'},
+        {'1', '1', '1', '1', '1'},
+        {'1', '0', '0', '1', '0'}
+    };
+
+    int maxRectangleArea = solution.maximalRectangle(matrix);
+    cout << "Maximal rectangle area: " << maxRectangleArea << endl;
+
+    return 0;
+}
---
+++ linkedList/CPP/86. Partition List.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode* low_than_x_head = new ListNode(); // Dummy node for values less than x
+        ListNode* high_than_x_head = new ListNode(); // Dummy node for values greater than or equal to x
+        ListNode* low = low_than_x_head;
+        ListNode* high = high_than_x_head;
+
+        while (head) {
+            if (head->val < x) {
+                low->next = head;
+                low = low->next;
+            } else {
+                high->next = head;
+                high = high->next;
+            }
+            head = head->next;
+        }
+        
+        high->next = nullptr; // Terminate the high_than_x list
+        low->next = high_than_x_head->next; // Connect the low_than_x list to high_than_x list
+
+        ListNode* result = low_than_x_head->next;
+        delete low_than_x_head; // Clean up the dummy node
+        delete high_than_x_head; // Clean up the dummy node
+        
+        return result;
+    }
+};
+
+
+// Helper function to create a linked list from an array
+ListNode* createLinkedListFromArray(int arr[], int n) {
+    if (n == 0) return nullptr;
+    ListNode* head = new ListNode(arr[0]);
+    ListNode* current = head;
+    for (int i = 1; i < n; ++i) {
+        current->next = new ListNode(arr[i]);
+        current = current->next;
+    }
+    return head;
+}
+
+// Helper function to print a linked list
+void printLinkedList(ListNode* head) {
+    while (head) {
+        std::cout << head->val << " ";
+        head = head->next;
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    Solution2 solution;
+    int arr[] = {1, 4, 3, 2, 5, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    ListNode* originalList = createLinkedListFromArray(arr, n);
+    std::cout << "Original list: ";
+    printLinkedList(originalList);
+
+    int x = 3;
+    ListNode* partitionedList = solution.partition(originalList, x);
+    std::cout << "Partitioned list with x=" << x << ": ";
+    printLinkedList(partitionedList);
+
+    // Clean up the allocated memory
+    while (partitionedList) {
+        ListNode* temp = partitionedList;
+        partitionedList = partitionedList->next;
+        delete temp;
+    }
+
+    return 0;
+}

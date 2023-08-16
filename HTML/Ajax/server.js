e6b55f11888536f62aaebe8644cb11f59f553643
ZadeAbhishek <zabhidoc@gmail.com>
2023-08-16 16:44:19 +0000

    Form html Update on Wed Aug 16 16:44:19 UTC 2023

---
+++ HTML/Ajax/server.js
@@ -0,0 +1,30 @@
+// Import required dependencies
+const express = require('express'); // Express.js for creating the server
+const cors = require('cors'); // CORS middleware for handling cross-origin requests
+const bp = require('body-parser'); // Body-parser middleware for parsing request bodies
+
+// Initialize the Express server
+const server = express();
+
+// Use CORS middleware to allow cross-origin requests
+server.use(cors());
+
+// Use body-parser middleware to parse JSON request bodies
+server.use(bp.json());
+
+// Start the server and listen on port 8080
+server.listen(8080, () => {
+    console.log("Listening on Port 8080....");
+});
+
+// Handle POST API request at endpoint '/userdata'
+server.post("/userdata", (req, res) => {
+    // Log the data received in the request body
+    console.log(req.body);
+
+    // Check if the request is defined (not undefined)
+    if (req != undefined)
+        res.send("Submitted"); // Send response "Submitted" if the request is defined
+    else
+        res.send("Error"); // Send response "Error" if the request is undefined
+});
--- StudyPDFs/README.md
+++ StudyPDFs/README.md
@@ -1,5 +1,6 @@
 # IMPORTANT LINK TO STUDY
 ## 🔗 Links
+
 [[Referral Docs]](https://docs.google.com/spreadsheets/u/0/d/1QDX8j0DUknjkuDvRyPTMK4v8wD6_4Sdi80TWFN_tbGw/htmlview)
 [[MQTT imp question]](https://climbtheladder.com/mqtt-interview-questions/)
 [[Big Data]](https://www.interviewbit.com/big-data-interview-questions/)

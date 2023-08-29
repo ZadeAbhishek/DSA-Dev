e4ffb88927904903ed11f5ad0352a42bf9349e34
ZadeAbhishek <zabhidoc@gmail.com>
2023-08-29 03:29:59 +0000

    Update on Tue Aug 29 03:29:59 UTC 2023

--- Array_and_Hashing/CPP/2483. Minimum Penalty for a Shop.cpp
+++ Array_and_Hashing/CPP/2483. Minimum Penalty for a Shop.cpp
@@ -1,14 +1,37 @@
+#include <iostream>
+#include <string>
+
 class Solution {
 public:
-    int bestClosingTime(string customers) {
+    int bestClosingTime(std::string customers) {
         int max_score = 0, score = 0, best_hour = -1;
-        for(int i = 0; i < customers.size(); ++i) {
+
+        // Iterate through each hour
+        for (int i = 0; i < customers.size(); ++i) {
+            // Update the score based on the customer type ('Y' or 'N')
             score += (customers[i] == 'Y') ? 1 : -1;
-            if(score > max_score) {
+
+            // Check if the current score is higher than the maximum score
+            if (score > max_score) {
                 max_score = score;
                 best_hour = i;
             }
         }
+
+        // Return the best closing hour (add 1 to convert from 0-based index to 1-based index)
         return best_hour + 1;
     }
 };
+
+int main() {
+    Solution solution;
+    
+    // Example usage:
+    std::string customers = "YYYNNYNNYNNYY";
+    int bestHour = solution.bestClosingTime(customers);
+
+    // Display the best closing hour
+    std::cout << "The best closing hour is at: " << bestHour << " o'clock." << std::endl;
+    
+    return 0;
+}

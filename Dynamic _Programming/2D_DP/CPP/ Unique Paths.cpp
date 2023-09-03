61dd5095269e83d1cc1accea5c4ceed0e7c3927a
ZadeAbhishek <zabhidoc@gmail.com>
2023-09-03 07:15:49 +0530

    Unique Psth added

--- Dynamic _Programming/2D_DP/CPP/ Unique Paths.cpp
+++ Dynamic _Programming/2D_DP/CPP/ Unique Paths.cpp
@@ -21,3 +21,32 @@ public:
          return uniquePaths(m,n,0,0,memo);
     }
 };
+
+class Solution {
+    
+public:
+    int uniquePaths(int m, int n) {
+         vector<vector<int>> memo(m+2,vector<int>(n+2,0));
+         memo[m][n] = 1;
+         for(int x = m ; x > 0 ; x--){
+             for(int y = n; y > 0 ; y--){
+                memo[x][y] += memo[x+1][y] + memo[x][y+1];
+             }
+         }
+         
+         return memo[1][1];
+    }
+};
+
+
+// explnation https://leetcode.com/problems/unique-paths/solutions/1581998/c-python-5-simple-solutions-w-explanation-optimization-from-brute-force-to-dp-to-math/
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        long ans = 1;
+        for(int i = m+n-2, j = 1; i >= max(m, n); i--, j++) 
+            ans = (ans * i) / j;
+        return ans;
+    }
+};
